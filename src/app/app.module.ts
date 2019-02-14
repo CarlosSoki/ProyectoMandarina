b8b12264da1b299b5ae660ca765d26e28f90b672
CarlosSoki <carlosfreitas@correo.unimet.edu.ve>
2019-02-13 21:31:52 -0400

    login agregado

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { DetailsBookComponent } from './components/details-book/details-book.component';
+import { ListBooksComponent } from './components/admin/list-books/list-books.component';
+import { Page404Component } from './components/page404/page404.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProfileComponent,
+    HeroComponent,
+    HomeComponent,
+    OffersComponent,
+    DetailsBookComponent,
+    ListBooksComponent,
+    Page404Component,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [AngularFireAuth],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
